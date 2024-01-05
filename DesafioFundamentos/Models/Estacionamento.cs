ace368abc2a0181be5de8c7d1e3f49e6dcbc6e3b
Alyff Antonio <alyffantonio@gmail.com>
2024-01-05 13:38:50 -0300

    Desafio Concluidogit add DesafioFundamentos/Models/Estacionamento.cs! git push origin main

--- DesafioFundamentos/Models/Estacionamento.cs
+++ DesafioFundamentos/Models/Estacionamento.cs
@@ -1,10 +1,14 @@
+using Microsoft.Win32.SafeHandles;
+
 namespace DesafioFundamentos.Models
 {
     public class Estacionamento
     {
         private decimal precoInicial = 0;
         private decimal precoPorHora = 0;
-        private List<string> veiculos = new List<string>();
+        private List<string> veiculos = new List<string>(); // Lista para armazenar as placas dos veículos estacionados
+        private int quantVeiculos;
+        private int limite = 2;
 
         public Estacionamento(decimal precoInicial, decimal precoPorHora)
         {
@@ -12,36 +16,44 @@ namespace DesafioFundamentos.Models
             this.precoPorHora = precoPorHora;
         }
 
-        public void AdicionarVeiculo()
+        public void AdicionarVeiculo()  // Método para adicionar um veículo ao estacionamento
         {
-            // TODO: Pedir para o usuário digitar uma placa (ReadLine) e adicionar na lista "veiculos"
-            // *IMPLEMENTE AQUI*
-            Console.WriteLine("Digite a placa do veículo para estacionar:");
+            // *IMPLEMENTADO*
+            
+            Console.WriteLine("Digite a placa do veículo para estacionar:");  // Solicitar ao usuário digitar uma placa e adicionar à lista "veiculos"
+              
+            string placa = Console.ReadLine();
+            
+            if (quantVeiculos > limite)
+            {
+                Console.WriteLine("Execedeu o Limite de Veiculos no estacionamento!");
+                
+            }
+            else
+            {
+                veiculos.Add(placa);
+                quantVeiculos += 1 ;
+                limite -= 1;
+            }
         }
 
-        public void RemoverVeiculo()
+        public void RemoverVeiculo()  // Método para remover um veículo do estacionamento
         {
+
             Console.WriteLine("Digite a placa do veículo para remover:");
 
-            // Pedir para o usuário digitar a placa e armazenar na variável placa
-            // *IMPLEMENTE AQUI*
-            string placa = "";
-
-            // Verifica se o veículo existe
-            if (veiculos.Any(x => x.ToUpper() == placa.ToUpper()))
+            string placa = Console.ReadLine();  // Pedir ao usuário digitar a placa e armazenar na variável placa
+            
+            if (veiculos.Any(x => x.ToUpper() == placa.ToUpper()))  // Verifica se o veículo existe
             {
                 Console.WriteLine("Digite a quantidade de horas que o veículo permaneceu estacionado:");
 
-                // TODO: Pedir para o usuário digitar a quantidade de horas que o veículo permaneceu estacionado,
-                // TODO: Realizar o seguinte cálculo: "precoInicial + precoPorHora * horas" para a variável valorTotal                
-                // *IMPLEMENTE AQUI*
-                int horas = 0;
-                decimal valorTotal = 0; 
+                decimal valorTotal = int.TryParse(Console.ReadLine(), out int horas)? precoInicial + (precoPorHora * horas): 0 ;
+                
+                quantVeiculos -=1;  limite += 1;
 
-                // TODO: Remover a placa digitada da lista de veículos
-                // *IMPLEMENTE AQUI*
+                Console.WriteLine(valorTotal > 0 && veiculos.Remove(placa) ? $"O veículo {placa} foi removido e o preço total foi de: R$ {valorTotal}" : "Por favor, insira um valor válido para as horas." );
 
-                Console.WriteLine($"O veículo {placa} foi removido e o preço total foi de: R$ {valorTotal}");
             }
             else
             {
@@ -49,14 +61,19 @@ namespace DesafioFundamentos.Models
             }
         }
 
-        public void ListarVeiculos()
+        public void ListarVeiculos()  // Método para listar os veículos estacionados
         {
-            // Verifica se há veículos no estacionamento
-            if (veiculos.Any())
+            if (veiculos.Any())  // Verificar se há veículos no estacionamento
             {
-                Console.WriteLine("Os veículos estacionados são:");
-                // TODO: Realizar um laço de repetição, exibindo os veículos estacionados
-                // *IMPLEMENTE AQUI*
+                Console.WriteLine($"Veiculos estacionados: {quantVeiculos}" );
+                
+                // *IMPLEMENTADO!
+                
+                foreach(string x in veiculos) // Iterar sobre a lista e exibir cada veículo
+                {
+                    Console.WriteLine($"Veiculo:{x}");
+                }
+                Console.WriteLine(limite == 0 ? $"Não há vagas disponiveis" : $"vagas disponiveis:{limite}");
             }
             else
             {
@@ -65,3 +82,33 @@ namespace DesafioFundamentos.Models
         }
     }
 }
+
+
+
+
+
+            /*if (veiculos.Any(x => x.ToUpper() == placa.ToUpper()))  // Verifica se o veículo existe
+            {
+                Console.WriteLine("Digite a quantidade de horas que o veículo permaneceu estacionado:");
+                
+                // *IMPLEMENTADO
+                
+                if (int.TryParse(Console.ReadLine(),out int horas))   // Tenta converter a entrada do usuário em um valor inteiro        
+                {
+                    decimal valorTotal = precoInicial + (precoPorHora * horas);  // Calcular o valor total
+
+                    // *IMPLEMENTADO!*
+
+                    veiculos.Remove(placa);  // Remover o veículo da lista
+                
+                    Console.WriteLine($"O veículo {placa} foi removido e o preço total foi de: R$ {valorTotal}");  // Exibir mensagem de remoção
+                }
+                else
+                {
+                    Console.WriteLine("Por favor, insira um valor válido para as horas.");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Desculpe, esse veículo não está estacionado aqui. Confira se digitou a placa corretamente");
+            }*/
